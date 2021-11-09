ee808a02982386349aaeafb65ced9dd15d542110
Daniel <wuyh512@foxmail.com>
2021-11-09 15:57:31 +0800

    配置热加载

--- main.go
+++ main.go
@@ -12,5 +12,9 @@ func main() {
 		c.String(http.StatusOK, "获取帖子ID为 %s", c.Param("topic_id"))
 	})
 
-	router.Run(":8000") // 配置端口号
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "欢迎来到首页")
+	})
+
+	router.Run(":8080") // 配置端口号
 }
